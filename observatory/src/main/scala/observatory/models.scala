a8f1fd1714825fc8a4190b0fcc17753637fa8774
hung-phan <colorvisavn@gmail.com>
2017-05-20 17:29:51 +0700

    Update Interaction

--- observatory/src/main/scala/observatory/models.scala
+++ observatory/src/main/scala/observatory/models.scala
@@ -10,6 +10,14 @@ case class Location(lat: Double, lon: Double) {
   lazy val point: Point = Point(toRadians(lat), toRadians(lon))
 }
 
+case class Tile(x: Double, y: Double, zoom: Int) {
+  lazy val location: Location = Location(
+    lat = toDegrees(atan(sinh(Pi * (1.0 - 2.0 * y / (1 << zoom))))),
+    lon = x / (1 << zoom) * 360.0 - 180.0)
+
+  def toURI = new java.net.URI("http://tile.openstreetmap.org/" + zoom + "/" + x + "/" + y + ".png")
+}
+
 case class Color(red: Int, green: Int, blue: Int) {
   def pixel(alpha: Int = 255): Pixel = RGBColor(red, green, blue, alpha).toPixel
 }
